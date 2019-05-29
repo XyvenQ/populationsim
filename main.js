de618245b8914a65a8ce267de9e9c737d510ed6f
bbruell <bbruell2021@francisparker.org>
2019-05-29 08:30:14 -0700

    basic site, reset does not reset graph yet

--- main.js
+++ main.js
@@ -1,47 +1,148 @@
-var sun = new Image();
-var moon = new Image();
-var earth = new Image();
-function init() {
-  sun.src = 'https://mdn.mozillademos.org/files/1456/Canvas_sun.png';
-  moon.src = 'https://mdn.mozillademos.org/files/1443/Canvas_moon.png';
-  earth.src = 'https://mdn.mozillademos.org/files/1429/Canvas_earth.png';
-  window.requestAnimationFrame(draw);
+var cayoteImg = new Image(40, 40);
+var rabbitImg = new Image(40, 40);
+cayoteImg.src = "resources/cayote.png";
+rabbitImg.src = "resources/rabbit.png";
+var canvas = document.getElementById('canvas');
+var tick;
+var isRunning;
+var cayotes = 15;
+var rabbits = 140;
+var cpp = [];
+var rpp = [];
+var xVal = 0;
+var dataLength = 1000; 
+
+var a = 1;
+var b = .03125;
+var c = .16;
+var d = 20;
+var P = 400;
+var timeInc = .005;
+
+function start() {
+  if (isRunning)
+    return null;
+  update();
+  tick = setInterval(update, 0);
+  isRunning = true;
+}
+
+function reset() {
+  clearInterval(tick);
+  isRunning = false;
+  canvas.getContext('2d').clearRect(0, 0, canvas.width, canvas.height);
+  updateInput();
+  //updateChart();
+}
+
+function resetChart(){
+  chart.options.data[0].dataPoints = [];
+  chart.options.data[1].dataPoints = [];
+  cpp=[];
+  rpp=[];
+  xVal = 0;
+  updateChart();
+}
+function pause() {
+  clearInterval(tick);
+  isRunning = false;
+}
+
+function update() {
+  updatePop();
+  updateChart();
+  if(document.getElementById("render").checked){
+    window.requestAnimationFrame(draw);
+  }
+}
+
+function updateInput() {
+  cayotes = parseInt(document.getElementById("startPopCayote").value, 10);
+  rabbits = parseInt(document.getElementById("startPopRabbit").value, 10);
+  update();
+}
+
+function updatePop() {
+  var dCayotePop = c*rabbits*cayotes-d*cayotes;
+  var dRabbitPop = a*(rabbits*(P-rabbits)/P)-b*rabbits*cayotes;
+  if(cayotes<1)
+    cayotes=0;
+  else  
+    cayotes += dCayotePop*timeInc;
+  if(rabbits<1)
+    rabbits=0;
+  
+    rabbits += dRabbitPop*timeInc;
 }
 
 function draw() {
-  var ctx = document.getElementById('canvas').getContext('2d');
-
-  ctx.globalCompositeOperation = 'destination-over';
-  ctx.clearRect(0, 0, 300, 300); // clear canvas
-
-  ctx.fillStyle = 'rgba(0, 0, 0, 0.4)';
-  ctx.strokeStyle = 'rgba(0, 153, 255, 0.4)';
+  var ctx = canvas.getContext('2d');
+  ctx.canvas.width = canvas.clientWidth;
+  ctx.canvas.height = canvas.clientWidth / 2;
+  ctx.clearRect(0, 0, canvas.width, canvas.height);
   ctx.save();
-  ctx.translate(150, 150);
-
-  // Earth
-  var time = new Date();
-  ctx.rotate(((2 * Math.PI) / 60) * time.getSeconds() + ((2 * Math.PI) / 60000) * time.getMilliseconds());
-  ctx.translate(105, 0);
-  ctx.fillRect(0, -12, 40, 24); // Shadow
-  ctx.drawImage(earth, -12, -12);
-
-  // Moon
-  ctx.save();
-  ctx.rotate(((2 * Math.PI) / 6) * time.getSeconds() + ((2 * Math.PI) / 6000) * time.getMilliseconds());
-  ctx.translate(0, 28.5);
-  ctx.drawImage(moon, -3.5, -3.5);
+  for (let index = 0; index < Math.floor(cayotes); index++) {
+    ctx.drawImage(cayoteImg, randomRange(0, ctx.canvas.width - 80), randomRange(0, ctx.canvas.height - 80), 80, 80);
+  }
+  for (let index = 0; index < Math.floor(rabbits); index++) {
+    ctx.drawImage(rabbitImg, randomRange(0, ctx.canvas.width - 40), randomRange(0, ctx.canvas.height - 40), 40, 40);
+  }
   ctx.restore();
-
-  ctx.restore();
-  
-  ctx.beginPath();
-  ctx.arc(150, 150, 105, 0, Math.PI * 2, false); // Earth orbit
-  ctx.stroke();
- 
-  ctx.drawImage(sun, 0, 0, 300, 300);
-
-  window.requestAnimationFrame(draw);
 }
 
-init();
+function randomRange(min, max) {
+  return Math.random() * (max - min) + min;
+}
+
+var chart = new CanvasJS.Chart("chartContainer", {
+  title: {
+    text: "Population"
+  },
+  axisY: {
+    includeZero: true,
+    name: "Rabbit Pop",
+    minimum: 0
+  },
+  axisY2: {
+    includeZero: true,
+    name: "Cayote Pop",
+    minimum: 0
+  },
+  axisX:{
+    name: "Years"
+  },
+  data: [{
+      type: "spline",
+      dataPoints: rpp
+    },
+    {
+      type: "spline",
+      dataPoints: cpp,
+      axisYType: "secondary"
+    }
+  ]
+});
+
+function updateChart(count) {
+
+  count = count || 1;
+
+  for (var j = 0; j < count; j++) {
+    cpp.push({
+      x: xVal,
+      y: cayotes,
+    });
+    rpp.push({
+      x: xVal,
+      y: rabbits,
+    });
+    xVal++;
+  }
+
+  if (rpp.length > dataLength) {
+    rpp.shift();
+    cpp.shift();
+  }
+
+  chart.render();
+};
